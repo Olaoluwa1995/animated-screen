0ddfd493775b3332a7997d415f46b7b8a59f0c0d
Olaoluwa1995 <olakulehinstephen@gmail.com>
2021-09-13 20:06:05 +0100

    Created app colors

---
+++ lib/constants/app-colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+/// App Colors Class - Resource class for storing app level color constants
+class AppColors {
+  static const Color PRIMARY_COLOR = Color(0xFF0066CC);
+  static const Color NAVY_BLUE_COLOR = Color(0xFF00008B);
+}
